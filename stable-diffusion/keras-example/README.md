cf3e941eddf1184ec6509c4d5a7152d389cc7bdc
pangyuteng <pangyuteng@gmail.com>
2022-11-05 05:48:35 +0000

    added keras example

--- stable-diffusion/keras-example/README.md
+++ stable-diffusion/keras-example/README.md
@@ -1,6 +1,10 @@
 
-# https://keras.io/guides/keras_cv/generate_images_with_stable_diffusion/
+```
+
+https://keras.io/guides/keras_cv/generate_images_with_stable_diffusion
 
 docker build -t keras-stable-diffusion .
 
-docker run -it -u $(id -u):$(id -g) -w $PWD -v /mnt:/mnt keras-stable-diffusion bash
+docker run --runtime=nvidia -it -u $(id -u):$(id -g) -w $PWD -v /mnt:/mnt keras-stable-diffusion bash
+
+```
--- stable-diffusion/keras-example/example.py
+++ stable-diffusion/keras-example/example.py
@@ -3,19 +3,17 @@
 import time
 import keras_cv
 from tensorflow import keras
-import matplotlib.pyplot as plt
+import imageio
 
+BATCH_SIZE = 3
+text = """
+artificial intelligence gone wrong, watercolor
+"""
+text = text.replace("\n","")
+print(text)
 
 model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
-images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=3)
+images = model.text_to_image(text,batch_size=BATCH_SIZE)
+for x in range(BATCH_SIZE):
+    imageio.imwrite(f'sample_{x:05d}.png',images[x,:])
 
-def plot_images(images):
-    plt.figure(figsize=(20, 20))
-    for i in range(len(images)):
-        ax = plt.subplot(1, len(images), i + 1)
-        plt.imshow(images[i])
-        plt.axis("off")
-
-# def main():
-# if __name__ == "__main__":
-#     main()
