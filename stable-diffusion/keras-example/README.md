fc5ff4abedb345ba4c07bfc48984e4c29281c979
pangyuteng <pangyuteng@gmail.com>
2022-11-06 05:48:17 +0000

    fun stuff

--- stable-diffusion/keras-example/README.md
+++ stable-diffusion/keras-example/README.md
@@ -7,8 +7,9 @@ docker build -t keras-stable-diffusion .
 
 docker run --runtime=nvidia -it -u $(id -u):$(id -g) -w $PWD -v /mnt:/mnt keras-stable-diffusion bash
 
-python example.py
+python demo.py "artificial intelligence gone wrong, watercolor" 2022-11-05
 
 cd /opt/stable-diffusion-tensorflow
+python interactive_text2image.py --mp --jit
 
 ```
--- stable-diffusion/keras-example/demo.py
+++ stable-diffusion/keras-example/demo.py
@@ -4,16 +4,20 @@ import time
 import keras_cv
 from tensorflow import keras
 import imageio
+import sys
+import os
+
+text = sys.argv[1]
+folder = sys.argv[2]
+
+os.makedirs(folder)
+print(text)
+print(folder)
 
 BATCH_SIZE = 5
-text = """
-artificial intelligence gone wrong, watercolor
-"""
-text = text.replace("\n","")
-print(text)
-
 model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
 images = model.text_to_image(text,batch_size=BATCH_SIZE)
 for x in range(BATCH_SIZE):
-    imageio.imwrite(f'sample_{x:05d}.png',images[x,:])
+    file_path = os.path.join(folder,f'sample_{x:05d}.png')
+    imageio.imwrite(file_path,images[x,:])
 
