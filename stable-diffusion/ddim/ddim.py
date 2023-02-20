296e89b70d7699b3b413445aba762d6cade433aa
pangyuteng <pangyuteng@gmail.com>
2023-02-20 00:08:05 +0000

    reduced image size

--- stable-diffusion/ddim/ddim.py
+++ stable-diffusion/ddim/ddim.py
@@ -21,7 +21,8 @@ from keras import layers
 dataset_name = "oxford_flowers102"
 dataset_repetitions = 5
 num_epochs = 50  # train for at least 50 epochs for good results
-image_size = 256
+image_size = 64
+
 # KID = Kernel Inception Distance, see related section
 kid_image_size = 75
 kid_diffusion_steps = 5
@@ -38,7 +39,7 @@ widths = [32, 64, 96, 128]
 block_depth = 2
 
 # optimization
-batch_size = 8
+batch_size = 64
 ema = 0.999
 learning_rate = 1e-3
 weight_decay = 1e-4
