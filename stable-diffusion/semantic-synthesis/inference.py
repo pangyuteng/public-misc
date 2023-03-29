7a36d8ae1af64ce80db9e8c3817f1ea1f6cc7148
pangyuteng <pangyuteng@gmail.com>
2023-03-29 19:03:09 +0000

    added 2d inference.py

--- stable-diffusion/semantic-synthesis/inference.py
+++ stable-diffusion/semantic-synthesis/inference.py
@@ -8,15 +8,12 @@ from totalseg import (
     DiffusionModel, checkpoint_path,
     image_size, widths, block_depth,
     learning_rate, weight_decay,
-    prepare_dataset, batch_size,
+    prepare_dataset, batch_size, num_rows,num_cols,
     label_count, TMP_DIR,
 )
 if __name__ == "__main__":
 
     epoch = sys.argv[1]
-    label_file = sys.argv[2]
-    output_file = sys.argv[3]
-
     plot_diffusion_steps = 100
 
     norm_dataset, train_dataset , val_dataset = prepare_dataset()
@@ -34,32 +31,46 @@ if __name__ == "__main__":
     model.network.load_weights(f'{TMP_DIR}/network_{epoch}.h5')
     model.ema_network.load_weights(f'{TMP_DIR}/ema_network_{epoch}.h5')
 
-    label_obj = sitk.ReadImage(label_file)
-    labels = sitk.GetArrayFromImage(label_obj)
-    labels = labels.astype(np.float32)
-    original_shape = list(labels.shape)
-    new_shape = [original_shape[0],image_size,image_size]
-    labels = resize(labels,new_shape,order=0,preserve_range=True, anti_aliasing=False)
-    labels = labels/label_count
+    if len(sys.argv) == 2:
+        for images,labels in val_dataset.take(1):
+            model._labels = labels.numpy()
+            model.plot_images(epoch=999,num_rows=num_rows, num_cols=num_cols)
 
-    xhat = np.zeros_like(labels)
-    num_images = labels.shape[0]
-    initial_noise = tf.random.normal(shape=(1, image_size, image_size, 1))
-    for x in range(num_images):
-        print(f'{x} of {num_images}')
-        tmp_label = np.expand_dims(np.expand_dims(labels[x,:],axis=-1),axis=0)
-        generated_images = model.reverse_diffusion(initial_noise, plot_diffusion_steps, tmp_label)
-        generated_images = model.denormalize(generated_images)
-        xhat[x,:] = generated_images.numpy().squeeze()
+    if len(sys.argv) == 4:
+        label_file = sys.argv[2]
+        output_file = sys.argv[3]
 
-    xhat = resize(xhat,original_shape,order=1,preserve_range=True,anti_aliasing=True)
-    xhat = ((xhat*2000)-1000).astype(np.int16)
-    xhat_obj =  sitk.GetImageFromArray(xhat)
-    xhat_obj.CopyInformation(label_obj)
-    sitk.WriteImage(xhat_obj,output_file)
+        label_obj = sitk.ReadImage(label_file)
+        labels = sitk.GetArrayFromImage(label_obj)
+        labels = labels.astype(np.float32)
+        original_shape = list(labels.shape)
+        new_shape = [original_shape[0],image_size,image_size]
+        labels = resize(labels,new_shape,order=0,preserve_range=True, anti_aliasing=False)
+        labels = labels/label_count
+
+        xhat = np.zeros_like(labels)
+        num_images = labels.shape[0]
+        initial_noise = tf.random.normal(shape=(1, image_size, image_size, 1))
+        for x in range(num_images):
+            print(f'{x} of {num_images}')
+            tmp_label = np.expand_dims(np.expand_dims(labels[x,:],axis=-1),axis=0)
+            generated_images = model.reverse_diffusion(initial_noise, plot_diffusion_steps, tmp_label)
+            generated_images = model.denormalize(generated_images)
+            xhat[x,:] = generated_images.numpy().squeeze()
+
+        xhat = resize(xhat,original_shape,order=1,preserve_range=True,anti_aliasing=True)
+        xhat = ((xhat*2000)-1000).astype(np.int16)
+        xhat_obj =  sitk.GetImageFromArray(xhat)
+        xhat_obj.CopyInformation(label_obj)
+        sitk.WriteImage(xhat_obj,output_file)
 
 """
 
 CUDA_VISIBLE_DEVICES=1 python inference.py 120 /mnt/scratch/data/Totalsegmentator_dataset/s1401/segmentations.nii.gz out.nii.gz
+
+
+../itksnap-4.0.0-20230220-Linux-gcc64/bin/itksnap -g ct.nii.gz -s segmentations.nii.gz
+
+../itksnap-4.0.0-20230220-Linux-gcc64/bin/itksnap -g out.nii.gz -s segmentations.nii.gz
 
 """
