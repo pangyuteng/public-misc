178a87b2eb32c5e224b6faf84999dc92dc023804
pangyuteng <pangyuteng@gmail.com>
2023-04-09 20:51:21 +0000

    added condor_submit script

---
+++ stable-diffusion/ct/build_and_push.sh
@@ -0,0 +1,3 @@
+docker build -t keras-stable-diffusion .
+docker tag keras-stable-diffusion pangyuteng/keras-stable-diffusion:latest 
+docker push pangyuteng/keras-stable-diffusion:latest
---
+++ stable-diffusion/ct/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /mnt/hd1/aigonewrong/stable-diffusion/ct
+python vqvae.py
