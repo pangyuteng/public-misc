a4ef9e719f2e62df231956773cb09bc1c0c71cfe
pangyuteng <pangyuteng@gmail.com>
2023-12-01 21:51:11 -0800

    moved static folders to .sh file

--- stable-diffusion/semantic-synthesis/cityscapes/run.sh
+++ stable-diffusion/semantic-synthesis/cityscapes/run.sh
@@ -3,5 +3,6 @@
 export TFDS_DATA_DIR=/radraid/pteng-public/tmp/tensorflow_datasets
 export CHECKPOINT_PATH=/cvibraid/cvib2/apps/personal/pteng/github/aigonewrong/stable-diffusion/semantic-synthesis/cityscapes
 export TEMP_PATH=/cvibraid/cvib2/apps/personal/pteng/github/aigonewrong/stable-diffusion/semantic-synthesis/cityscapes/tmp
-#python3 cityscapes.py
+
+python3 cityscapes.py
 python3 ddim.py
