df4eb89cf8bf84972b66e2574ccac33039370a64
pangyuteng <pangyuteng@gmail.com>
2023-12-04 13:55:11 -0800

    added inference*

---
+++ stable-diffusion/semantic-synthesis/cityscapes/inference.py
@@ -0,0 +1,16 @@
+import keras
+from ddm import DiffusionModel,checkpoint_path
+
+model = DiffusionModel(image_size, widths, block_depth)
+model.network.summary()
+
+model.compile(
+    optimizer=keras.optimizers.experimental.AdamW(
+        learning_rate=learning_rate, weight_decay=weight_decay
+    ),
+    loss=keras.losses.mean_absolute_error,
+    run_eagerly=True
+)
+# load the best model and generate images
+model.load_weights(checkpoint_path)
+model.plot_images(epoch=999)
