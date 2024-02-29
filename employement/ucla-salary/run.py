41162659d80d9b7b9719cf29af8a8c3e4caaab6f
pangyuteng <pangyuteng@gmail.com>
2024-02-29 23:03:12 +0000

    updated to plot per salary levels

--- employement/ucla-salary/run.py
+++ employement/ucla-salary/run.py
@@ -17,7 +17,6 @@ if not os.path.exists(fname):
     csv_file_list = [str(x) for x in Path(".").rglob("*.csv")]
     for csv_file in csv_file_list:
         item = pd.read_csv(csv_file)
-        item = item.iloc[:1000,:]
         df_list.append(item)
     df = pd.concat(df_list)
     columns = ["Base Pay","Overtime Pay","Other Pay","Benefits","Total Pay","Pension Debt","Total Pay & Benefits"]
@@ -27,21 +26,43 @@ if not os.path.exists(fname):
 
 sns.set_theme(style="darkgrid")
 df = pd.read_parquet(fname)
+print(df.shape)
 
-df["JobTitle"] = df["Job Title"].apply(lambda x: ''.join([i for i in x if not i.isdigit()]) )
 df.Year = df.Year.apply(lambda x: int(x))
 df["TotalPay"] = df["Total Pay & Benefits"].apply(lambda x: int(x))
+# remove level from job title.
+df["JobTitle"] = df["Job Title"].apply(lambda x: ''.join([i for i in x if not i.isdigit()]) )
+df["JobTitle"] = df["Job Title"].apply(lambda x: x.replace(' ',''))
+
+# weed out part time rolls via removing total comp to be less than 60k
+df['StartingSalaryLevel'] = None
+min_year = df.Year.min()
+baseline_df = df[df.Year == min_year]
+salary_range = {
+    '50k-80k':(50000,80000),
+    '80k-120k':(80000,120000),
+    '120k-250k':(120000,250000),
+    '250k-400k':(250000,400000),
+    '400k+':(400000,5000000),
+}
+
+print(baseline_df.TotalPay.min(),baseline_df.TotalPay.max())
+
+for salary_level,range_min_max in salary_range.items():
+    min_val,max_val = range_min_max
+    jobdf = baseline_df[(baseline_df.TotalPay>min_val)&(baseline_df.TotalPay<max_val)]
+    for x in jobdf.JobTitle.unique():
+        df.loc[df.JobTitle==x,"StartingSalaryLevel"]=salary_level
+        
+    print(salary_level,jobdf.TotalPay.median(),len(jobdf))
+
 sns.set(rc={'figure.figsize':(20,20)})
-#sns.lineplot(data=df, x="Year", y="TotalPay", hue="JobTitle")
 sns.relplot(
     data=df,
     x="Year",
     y="TotalPay",
-    hue="JobTitle",
+    hue="StartingSalaryLevel",
     kind="line",
     facet_kws={"legend_out": True}
 )
 plt.savefig('time-salary.png')
-
-# plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.3)
-# plt.savefig('time-salary.png')
