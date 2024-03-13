05403123355a9e0a9cad8bd09249b15fc88c626b
pangyuteng <pangyuteng@gmail.com>
2024-03-13 02:36:50 +0000

    wip

---
+++ finance/btc/hohoho.py
@@ -0,0 +1,22 @@
+import os
+import datetime
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = yf.download("btc-usd mstr aapl qqq tsla nvda smci",
+    start="2020-07-28",end="2024-03-12")
+df = data['Adj Close']
+df = df.dropna()
+for x in df.columns:
+    if x == 'Date':
+        continue
+    tmp = df[x]/df['BTC-USD']
+    tmp /= tmp[0]
+    plt.plot(df.index,tmp,label=x)
+plt.legend()
+plt.grid(True)
+plt.savefig('hohoho.png')
+
+# https://twitter.com/PrestonPysh/status/1767642324954136958
