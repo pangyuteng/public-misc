452b6d22d57d843231aae086335b45fa2707cc46
pangyuteng <pangyuteng@gmail.com>
2024-03-16 20:42:46 +0000

    wip

--- employement/ucla-salary/job_title_to_dict.py
+++ employement/ucla-salary/job_title_to_dict.py
@@ -1,9 +1,10 @@
 import os
 import json
 import pandas as pd
+import numpy as np
 
 job_txt_file = 'job_list.txt'
-
+word2vec_file = 'word2vec.json'
 
 if not os.path.exists(job_txt_file):
     rawfname = 'raw-uc-salary.parquet.gzip'
@@ -14,72 +15,89 @@ if not os.path.exists(job_txt_file):
         for x in job_list:
             f.write(f'{x}\n')
 
-with open(job_txt_file,'r') as f:
-    job_list = [x for x in f.read().split('\n') if len(x)>0]
+if not os.path.exists(word2vec_file):
+    with open(job_txt_file,'r') as f:
+        job_list = [x for x in f.read().split('\n') if len(x)>0]
 
-# https://www.tensorflow.org/text/tutorials/word2vec
-# Now, create a custom standardization function to lowercase the text and
-# remove punctuation.
-
-
-# Define the vocabulary size and the number of words in a sequence.
-
-tmp_list = []
-for x in job_list:
-    tmp_list.extend(x.split(' '))
-vocab_size = len(set(tmp_list))
-print(f'vocab_size {vocab_size}')
-
-sequence_length = max([len(x.split(' ')) for x in job_list])
-print(f'max_vec_len {sequence_length}')
-
-import io
-import re
-import string
-import tqdm
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers
-
-SEED = 42
-AUTOTUNE = tf.data.AUTOTUNE
-
-def custom_standardization(input_data):
-  lowercase = tf.strings.lower(input_data)
-  return tf.strings.regex_replace(lowercase,
-                                  '[%s]' % re.escape(string.punctuation), '')
+    # https://www.tensorflow.org/text/tutorials/word2vec
+    # Now, create a custom standardization function to lowercase the text and
+    # remove punctuation.
 
 
-# Use the `TextVectorization` layer to normalize, split, and map strings to
-# integers. Set the `output_sequence_length` length to pad all samples to the
-# same length.
-vectorize_layer = layers.TextVectorization(
-    standardize=custom_standardization,
-    max_tokens=vocab_size,
-    output_mode='int',
-    output_sequence_length=sequence_length)
+    # Define the vocabulary size and the number of words in a sequence.
 
-text_ds = tf.data.TextLineDataset(job_txt_file).filter(lambda x: tf.cast(tf.strings.length(x), bool))
-vectorize_layer.adapt(text_ds.batch(1024))
+    tmp_list = []
+    for x in job_list:
+        tmp_list.extend(x.split(' '))
+    vocab_size = len(set(tmp_list))
+    print(f'vocab_size {vocab_size}')
 
-inverse_vocab = vectorize_layer.get_vocabulary()
+    sequence_length = max([len(x.split(' ')) for x in job_list])
+    print(f'max_vec_len {sequence_length}')
 
-print(len(inverse_vocab))
-print(inverse_vocab[:20])
+    import io
+    import re
+    import string
+    import tensorflow as tf
+    from tensorflow.keras import layers
+
+    SEED = 42
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    def custom_standardization(input_data):
+        lowercase = tf.strings.lower(input_data)
+        return tf.strings.regex_replace(lowercase,
+                                    '[%s]' % re.escape(string.punctuation), '')
+
+    # Use the `TextVectorization` layer to normalize, split, and map strings to
+    # integers. Set the `output_sequence_length` length to pad all samples to the
+    # same length.
+    vectorize_layer = layers.TextVectorization(
+        standardize=custom_standardization,
+        max_tokens=vocab_size,
+        output_mode='int',
+        output_sequence_length=sequence_length)
+
+    text_ds = tf.data.TextLineDataset(job_txt_file).filter(lambda x: tf.cast(tf.strings.length(x), bool))
+    vectorize_layer.adapt(text_ds.batch(1024))
+
+    inverse_vocab = vectorize_layer.get_vocabulary()
+
+    print(len(inverse_vocab))
+    print(inverse_vocab[:20])
 
 
-text_vector_ds = text_ds.batch(1024).prefetch(AUTOTUNE).map(vectorize_layer).unbatch()
-sequences = list(text_vector_ds.as_numpy_iterator())
-print(len(sequences))
+    text_vector_ds = text_ds.batch(1024).prefetch(AUTOTUNE).map(vectorize_layer).unbatch()
+    sequences = list(text_vector_ds.as_numpy_iterator())
+    print(len(sequences))
 
-print(sequences[0])
-print(sequences[-1])
+    print(sequences[0])
+    print(sequences[-1])
 
-print(f'so many job titles, len: {len(job_list)}')
-print('vocab_size',vocab_size)
-print('sequence_length',sequence_length)
+    print(f'so many job titles, len: {len(job_list)}')
+    print('vocab_size',vocab_size)
+    print('sequence_length',sequence_length)
+    print(type(sequences[0]))
+    mydict = {}
+    for job_title,vec in zip(job_list,sequences):
+        mydict[job_title]=vec.tolist()
 
+    with open(word2vec_file,'w') as f:
+        f.write(json.dumps(mydict,indent=True,default=str,sort_keys=True))
 
+with open(word2vec_file,'r') as f:
+    word2vec = json.loads(f.read())
+
+from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import KMeans
+X = np.array(list(word2vec.values()))
+K = np.array(list(word2vec.keys()))
+print(X.shape)
+kmeans = KMeans(n_clusters=64).fit(X)
+Y = kmeans.labels_
+for ind in range(len(K)):
+    print(Y[ind],K[ind])
+        
 # use knn to classify job to 32 categories ??
 # word2vec
 
