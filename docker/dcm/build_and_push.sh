dddb42efea786d7fce9869859b5be5ae8b8d554b
pangyuteng <pangyuteng@gmail.com>
2025-01-28 17:21:12 +0000

    updated base image for dcm

--- docker/dcm/build_and_push.sh
+++ docker/dcm/build_and_push.sh
@@ -3,5 +3,5 @@
 docker build -t pangyuteng/dcm:latest .
 docker push pangyuteng/dcm:latest
 
-docker build -t pangyuteng/dcm:minimal -f Dockerfile.minimal .
-docker push pangyuteng/dcm:minimal
+#docker build -t pangyuteng/dcm:minimal -f Dockerfile.minimal .
+#docker push pangyuteng/dcm:minimal
